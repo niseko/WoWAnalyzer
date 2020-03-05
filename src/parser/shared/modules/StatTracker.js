b4372300a3cdd7d545f8cc3addca7b9228ae1617
Joscha Rentz <rentz@hotmail.de>
2020-03-05 13:42:10 +0100

    calculate average intellect gain from flash of insight

--- src/parser/shared/modules/StatTracker.js
+++ src/parser/shared/modules/StatTracker.js
@@ -679,7 +679,7 @@ class StatTracker extends Analyzer {
   on_toPlayer_heal(event) {
     this._updateIntellect(event);
   }
-  _count = 0;
+
   _updateIntellect(event) {
     // updates intellect values directly from game events
     if (!event.spellPower) {
--- src/parser/shared/modules/spells/bfa/corruptions/FlashOfInsight.tsx
+++ src/parser/shared/modules/spells/bfa/corruptions/FlashOfInsight.tsx
@@ -1,7 +1,17 @@
-import SPELLS from 'common/SPELLS/index';
-import Events, { ApplyBuffStackEvent, RemoveBuffStackEvent } from 'parser/core/Events';
+import React from 'react';
+
 import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
+import Events, { ApplyBuffStackEvent, RemoveBuffStackEvent, ChangeStatsEvent, FightEndEvent } from 'parser/core/Events';
 import StatTracker from 'parser/shared/modules/StatTracker';
+
+import SPELLS from 'common/SPELLS/index';
+import ITEMS from 'common/ITEMS';
+import SpellLink from 'common/SpellLink';
+
+import Statistic from 'interface/statistics/Statistic';
+import PrimaryStatIcon from 'interface/icons/PrimaryStat';
+import { STATISTIC_CATEGORY } from 'interface/others/StatisticBox';
+import BoringItemValueText from 'interface/statistics/components/BoringItemValueText';
 
 // https://www.warcraftlogs.com/reports/tq3D9ajfr24nbHFX#fight=3&type=auras&source=18&view=events 
 
@@ -13,6 +23,9 @@ class FlashOfInsight extends Analyzer {
 
   statAmount: number = 0;
   stack: number = 0;
+  timestamp: number = 0;
+  currentGain: number = 0;
+  public historyBySpellId: { [amount: number]: number; } = {};
 
   constructor(options: any) {
     super(options);
@@ -23,6 +36,8 @@ class FlashOfInsight extends Analyzer {
 
     this.addEventListener(Events.applybuffstack.by(SELECTED_PLAYER).spell(SPELLS.FLASH_OF_INSIGHT_BUFF), this.stackChange);
     this.addEventListener(Events.removebuffstack.by(SELECTED_PLAYER).spell(SPELLS.FLASH_OF_INSIGHT_BUFF), this.stackChange);
+    this.addEventListener(Events.ChangeStats.by(SELECTED_PLAYER), this.statChange);
+    this.addEventListener(Events.fightend, this.onFightEnd);
   }
 
   stackChange(event: ApplyBuffStackEvent | RemoveBuffStackEvent) {
@@ -30,33 +45,70 @@ class FlashOfInsight extends Analyzer {
     if (this.stack === 0) {
       this.statTracker.addStatMultiplier("intellect", (1 + event.stack / 100), false);
       this.stack = event.stack;
+      this.timestamp = event.timestamp;
       return;
     }
-    
+
+    const duration = event.timestamp - this.timestamp;
+    const amount = (this.statTracker.currentIntellectRating - this.statTracker.currentIntellectRating / (1 + (this.stack / 100)));
+    if (!this.historyBySpellId[amount]) {
+      this.historyBySpellId[amount] = 0;
+    }
+    this.historyBySpellId[amount] += duration;
+
     this.statTracker.removeStatMultiplier("intellect", (1 + (this.stack / 100)), true);
     this.statTracker.addStatMultiplier("intellect", (1 + (event.stack / 100)), true);
 
-    //if (event.stack > this.stack) { // THIS IS NOT THE SAME AS REMOVING AND THEN ADDING; FIX
-    //  this.log(`adding ${(event.stack - this.stack)}`);
-    //  this.statTracker.addStatMultiplier("intellect", (1 + ((event.stack - this.stack) / 100)), true);
-    //} else {
-    //  this.log(`removing ${(this.stack - event.stack)}`);
-    //  this.statTracker.removeStatMultiplier("intellect", (1 + ((this.stack - event.stack) / 100)), true);
-    //}
+    this.timestamp = event.timestamp;
     this.stack = event.stack;
   }
 
-  // TODO add statistic
+  statChange(event: ChangeStatsEvent) {
+    if (!event.delta || !event.delta.intellect || !this.stack) {
+      return;
+    }
 
-  //statistic() {
-  //  return (
-  //    <Statistic size="flexible" category={STATISTIC_CATEGORY.ITEMS}>
-  //      <BoringSpellValueText spell={SPELLS.FLASH_OF_INSIGHT_T1}>
-  //        <CriticalIcon /> {this.weightedBuffUptime.toFixed(2)} <small>average Stacks</small>
-  //      </BoringSpellValueText>
-  //    </Statistic>
-  //  );
-  //}
+    const duration = event.timestamp - this.timestamp;
+    if (!duration) {
+      return;
+    }
+    const amount = (this.statTracker.currentIntellectRating - this.statTracker.currentIntellectRating / (1 + (this.stack / 100)));
+    if (!this.historyBySpellId[amount]) {
+      this.historyBySpellId[amount] = 0;
+    }
+    this.historyBySpellId[amount] += duration;
+
+    this.timestamp = event.timestamp;
+  }
+
+  onFightEnd(event: FightEndEvent) {
+    const duration = event.timestamp - this.timestamp;
+    if (!duration) {
+      return;
+    }
+    const amount = (this.statTracker.currentIntellectRating - this.statTracker.currentIntellectRating / (1 + (this.stack / 100)));
+    if (!this.historyBySpellId[amount]) {
+      this.historyBySpellId[amount] = 0;
+    }
+    this.historyBySpellId[amount] += duration;
+  }
+
+  get avgGain() {
+    return Object.entries(this.historyBySpellId)
+      .filter(ms => ms[1] > 0).reduce((total, amount) => total + Number(amount[0]) * amount[1], 0)
+      / Object.values(this.historyBySpellId).reduce((total, ms) => total + ms);
+  }
+
+  statistic() {
+    return (
+      <Statistic size="flexible" category={STATISTIC_CATEGORY.ITEMS}>
+        <BoringItemValueText item={ITEMS.MARKOWA}>
+          <SpellLink id={SPELLS.FLASH_OF_INSIGHT.id} /><br />
+          <PrimaryStatIcon stat={"Intellect"} /> {this.avgGain.toFixed(0)} <small>average Intellect</small>
+        </BoringItemValueText>
+      </Statistic>
+    );
+  }
 }
 
 export default FlashOfInsight;
